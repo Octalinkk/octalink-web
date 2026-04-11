041491b78c3ab56cef1d2a48e7ea12dfb63890c3
Octalink <btfoctalink@gmail.com>
2026-04-11 22:15:40 +0200

    Base JS/CSS/Bootstrap Setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# octalink-web
+# Octalink's Website
 This host the files for my website. Gotta init branch first :p
---
+++ js/index.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("wasd")
+
+document.addEventListener("DOMContentLoaded", async function () {
+    
+    btn.textContent = "Saluuut"
+});
+
+btn.addEventListener("click", async function () {
+    
+    btn.textContent = "Saluuut"
+});
